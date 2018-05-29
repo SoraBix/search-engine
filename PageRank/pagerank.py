665f845796ff5faf3b665492440db25d925dbe0e
Soravis Taekasem <taekasem@usc.edu>
2018-05-29 16:48:13 -0700

    Added pagerank script

---
+++ PageRank/pagerank.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+G = nx.read_edgelist("edgeList.txt",create_using=nx.DiGraph())
+pr = nx.pagerank(G, alpha=0.85, personalization=None, max_iter=30, tol=1e-06, nstart=None, weight='weight', dangling=None)
+
+outputFileObject = open("external_pageRankFile.txt","w")
+
+for key in pr:
+	outputFileObject.write("/media/sf_Shared/NBC_News/HTML files/" + key + "=" + str( pr[key]))
+	outputFileObject.write("\n")
+
+outputFileObject.close()
